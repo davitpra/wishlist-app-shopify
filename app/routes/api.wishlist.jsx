ffe0bba28e0d59f7338ac9c411e745801c6623e6
davitpra <davitpra@Davit>
2024-05-23 22:07:30 -0400

    api wishlist working'

--- app/routes/api.wishlist.jsx
+++ app/routes/api.wishlist.jsx
@@ -1,4 +1,6 @@
 import { json } from "@remix-run/node";
+import db from "../db.server";
+import { cors } from 'remix-utils/cors';
 
 export async function loader() {
     // 
@@ -21,14 +23,37 @@ export async function loader() {
 export async function action({ request }) {
 
     const method = request.method;
+    
+    let data = await request.formData();
+    data = Object.fromEntries(data);
+
+    const custumerId = data.custumerId;
+    const productId = data.productId;
+    const shop = data.shop;
+
+    if (!custumerId || !productId || !shop) {
+        return json({ 
+            message: "Missing required fields. Required fields are customerId, productId, and shop.",
+            method: method
+        });
+    }
 
     if (method === "POST") {
-        let message = 'Product added to wishlist';
 
-        return json({
-            message,
-             method: "POST"
+        const wishlist = await db.wishlist.create({
+            data: {
+                custumerId,
+                productId,
+                shop,
+            },
         });
+
+        const response = json({
+            message: "Product added to wishlist",
+            method: "POST",
+            wishlist
+        });
+        return cors(request, response)
     }
 
     if (method === "PATCH") {
