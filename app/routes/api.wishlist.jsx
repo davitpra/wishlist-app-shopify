7ab5c8b365bb8b900cbc758c97a89cd2fb99d62a
davitpra <davitpra@Davit>
2024-05-24 16:46:02 -0400

    delete funtionality wishlist added

--- app/routes/api.wishlist.jsx
+++ app/routes/api.wishlist.jsx
@@ -2,44 +2,70 @@ import { json } from "@remix-run/node";
 import db from "../db.server";
 import { cors } from 'remix-utils/cors';
 
-export async function loader() {
-    // 
-    const wishlist = [
-        {
-        id: 1,
-        name: "Product 1",
-        price: 100,
-        },
-        {
-        id: 2,
-        name: "Product 2",
-        price: 200,
-        },
-    ];
+// get request: accept request with request: customerId, shop, productId.
+// read database and return wishlist items for that customer.
+export async function loader({ request }) {
+
+    // Get wishlist items for a specific customer
+    const url = new URL(request.url);
+    const custumerId = url.searchParams.get("custumerId");
+    const shop = url.searchParams.get("shop");
+    const productId = url.searchParams.get("productId");
+  
+    // If customerId, shop, productId is not provided, return an error message.
+    if(!custumerId || !shop || !productId) {
+        return json({
+          message: "Missing data. Required data: customerId, productId, shop",
+          method: "GET"
+        });
+      }
     
-    return json(wishlist);
+    // If customerId, shop, productId is provided, return wishlist items for that customer.
+    const wishlist = await db.wishlist.findMany({
+        where: {
+        custumerId: custumerId,
+        shop: shop,
+        productId: productId,
+        },
+    });
+
+    // Return wishlist items
+    const response = json({
+        ok: true,
+        message: "Success",
+        data: wishlist,
+    });
+    
+    // Return response
+    return cors(request, response);
 }
 
+// Expexted data comes from post request. If
+// customerID, productID, shop
 export async function action({ request }) {
 
-    const method = request.method;
-    
+    // Get data from request body and convert it to an object
     let data = await request.formData();
     data = Object.fromEntries(data);
 
     const custumerId = data.custumerId;
     const productId = data.productId;
     const shop = data.shop;
+    // here comes the action method (CREATE,DELETE)
+    const _action = data._action;
 
     if (!custumerId || !productId || !shop) {
         return json({ 
             message: "Missing required fields. Required fields are customerId, productId, and shop.",
-            method: method
+            method: _action
         });
     }
 
-    if (method === "POST") {
+    let response;
 
+    if (_action === "CREATE") {
+      // Handle POST request logic here
+      // For example, adding a new item to the wishlist
         const wishlist = await db.wishlist.create({
             data: {
                 custumerId,
@@ -48,29 +74,40 @@ export async function action({ request }) {
             },
         });
 
-        const response = json({
+        response = json({
             message: "Product added to wishlist",
-            method: "POST",
-            wishlist
+            method: _action,
+            wishlisted: true
         });
         return cors(request, response)
     }
 
-    if (method === "PATCH") {
+     //TODO: UPDATE THE WISHLIST PRODUCT
+    if (_action === "PATCH") {
         let message = 'Product updated in wishlist';
-
+   
         return json({
             message,
              method: "PATCH"
         });
     }
 
-    if (method === "DELETE") {
-        let message = 'Product removed from wishlist';
-        return json({
-            message,
-             method: "DELETE"
+    if (_action === "DELETE") {
+        // Handle DELETE request logic here (Not tested)
+        // For example, removing an item from the wishlist
+        await db.wishlist.deleteMany({
+            where: {
+            custumerId: custumerId,
+            shop: shop,
+            productId: productId,
+            },
         });
+        response = json({ 
+            message: "Product removed from your wishlist", 
+            method: _action, 
+            wishlisted: false 
+        });
+        return cors(request, response)
     }
 
     return json({ message: "Method not allowed" });
